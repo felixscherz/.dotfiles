a370f3e383016e78ef9a2a2f7d3657eaf0e973bb
Felix Scherz <felix.scherz@exxeta.com>
2022-10-11 12:08:42 +0200

    expand $HOME in lua to set coc-settigns

--- nvim/.config/nvim/after/plugin/coc.lua
+++ nvim/.config/nvim/after/plugin/coc.lua
@@ -3,3 +3,8 @@ vim.cmd("command! -nargs=0 SortImports :CocCommand python.sortImports")
 vim.cmd("xmap <leader>f  <Plug>(coc-format-selected)")
 vim.cmd("nmap <leader>f  <Plug>(coc-format-selected)")
 vim.cmd("command! -nargs=0 Prettier :CocCommand prettier.formatFile")
+
+
+local home = os.getenv("HOME")
+vim.cmd(string.format("call coc#config('python.pythonPath', '%s/.nvim-python3/bin/python3')", home))
+vim.cmd(string.format("call coc#config('python.formatting.yapfPath', '%s/.nvim-python3/bin/yapf')", home))
