b4fc58ca16067a43072593b56e79e706b765af0d
Felix Scherz <felix.scherz@exxeta.com>
2022-10-17 09:40:50 +0200

    change hotkeys

--- nvim/.config/nvim/after/plugin/keymaps/init.lua
+++ nvim/.config/nvim/after/plugin/keymaps/init.lua
@@ -30,3 +30,14 @@ nmap("<leader>ns", ":CocCommand python.sortImports<CR>")
 nnoremap("gdh", ":diffget //2<CR>")
 nnoremap("gdl", ":diffget //3<CR>")
 
+nnoremap("<leader>q", ":Spotify<CR>")
+
+
+local builtin = require('telescope.builtin')
+nnoremap("<leader>ff", builtin.find_files)
+nnoremap("<leader>fg", builtin.live_grep)
+nnoremap("<leader>fb", builtin.buffers)
+nnoremap("<leader>fh", builtin.help_tags)
+
+
+nmap("<leader>rn", "<Plug>(coc-rename)")
