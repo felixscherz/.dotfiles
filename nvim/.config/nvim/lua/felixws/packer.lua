605154044b37faa700b7a8c24e60cd0804be6f81
Felix Scherz <felix.scherz@exxeta.com>
2022-10-17 09:41:05 +0200

    new plugins

--- nvim/.config/nvim/lua/felixws/packer.lua
+++ nvim/.config/nvim/lua/felixws/packer.lua
@@ -5,13 +5,13 @@ return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'folke/tokyonight.nvim'
-  use 'joshdick/onedark.vim'
 
   -- vertical bars to show indentation
   use 'Yggdroot/indentLine'
 
-  -- statusline
-  use 'vim-airline/vim-airline'
+  -- statusline (maybe use lualine in the future?)
+  use 'itchyny/lightline.vim'
+
   -- filetree navigation
   use 'preservim/nerdtree'
   -- git status flags for nerdtree
@@ -20,6 +20,7 @@ return require('packer').startup(function()
   use 'jiangmiao/auto-pairs'
 
   use {'neoclide/coc.nvim', branch = 'release'}
+  use 'folke/lsp-colors.nvim'
 
   -- fuzzy finder telescope + plenary as dependency
   use 'nvim-lua/plenary.nvim'
@@ -47,5 +48,12 @@ return require('packer').startup(function()
 
   -- vimwiki for notetaking
   use 'vimwiki/vimwiki'
+
+  -- spotify integration with vim
+  use 'HendrikPetertje/vimify'
+
+  -- show markdown in browser
+  -- install without yarn or npm
+  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install'}
   end)
 
