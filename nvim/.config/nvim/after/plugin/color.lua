dfd6fc9dfb89a46c3da749b6d6dcf380aca6b500
Felix Scherz <felix.scherz@exxeta.com>
2022-10-17 09:38:45 +0200

    chnage colors for CocTypehints

--- nvim/.config/nvim/after/plugin/color.lua
+++ nvim/.config/nvim/after/plugin/color.lua
@@ -1,7 +1,15 @@
 vim.opt.background = "dark"
 
+vim.cmd("let g:lightline = {'colorscheme' : 'tokyonight'}")
 vim.cmd("colorscheme tokyonight")
 
-vim.cmd[[highlight ColoColumn ctermbg=0 guibg=lightgrey]]
+
+vim.cmd[[highlight CursorLine ctermbg=60 guibg=#363d59]]
+
+vim.cmd[[highlight CocInlayHint guifg=#f7768e guibg=#24283b]]
+vim.cmd[[highlight link CocInlayHintParameter CocInlayHint]]
+vim.cmd[[highlight link CocInlayHintType CocInlayHint]]
+
+
 
 vim.opt.cursorline = true
