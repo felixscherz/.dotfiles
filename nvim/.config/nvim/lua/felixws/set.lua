8aea4981f3986b175bb1a34777fa669bb86e8791
Felix Scherz <felix.scherz@exxeta.com>
2022-10-20 11:29:27 +0200

    enable show whitespace with :set list

--- nvim/.config/nvim/lua/felixws/set.lua
+++ nvim/.config/nvim/lua/felixws/set.lua
@@ -31,5 +31,12 @@ vim.g.mapleader = " "
 vim.opt.hidden = true
 
 vim.g.indentLine_conceallevel = 0
+vim.g.json_conceal = 0
+vim.g.markdown_syntax_conceal = 0
+vim.opt.conceallevel = 1
 
 vim.opt.clipboard = vim.opt.clipboard ^ {"unnamed", "unnamedplus"}
+
+vim.cmd[[
+set listchars=eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:␣
+]]
