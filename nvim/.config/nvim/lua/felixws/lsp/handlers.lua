6e72ef4322be6042c0f0285f35459ecf5e3d786d
Felix Scherz <felix.scherz@exxeta.com>
2022-10-31 09:14:55 +0100

    add signs after fixing font

--- nvim/.config/nvim/lua/felixws/lsp/handlers.lua
+++ nvim/.config/nvim/lua/felixws/lsp/handlers.lua
@@ -9,9 +9,9 @@ M.setup = function()
     { name = "DiagnosticSignInfo", text = "" },
   }
 
-  -- for _, sign in ipairs(signs) do
-  --   vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-  -- end
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  end
 
   local config = {
     -- disable virtual text
