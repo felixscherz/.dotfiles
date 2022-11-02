5ca764e22bee94f49227d24ca4f7abc70e950520
Felix Scherz <felix.scherz@exxeta.com>
2022-11-02 10:59:12 +0100

    move lualine config

--- nvim/.config/nvim/after/plugin/color.lua
+++ nvim/.config/nvim/after/plugin/color.lua
@@ -2,6 +2,14 @@ require("tokyonight").setup({
 	transparent = true,
 })
 
+require("lualine").setup({
+	options = {
+		-- ... your lualine config
+		theme = "tokyonight",
+		-- ... your lualine config
+	},
+})
+
 vim.cmd("colorscheme tokyonight")
 
 vim.api.nvim_set_hl(0, "Search", { link = "IncSearch" })
