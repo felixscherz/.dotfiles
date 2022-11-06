f8a3dcd4f3c7d8f5a022b66b1cf302f19f5a0736
Felix <felixwscherz@gmail.com>
2022-11-06 17:25:56 +0100

    leave neotree open if its the last buffer

--- nvim/.config/nvim/lua/felixws/plugins/neotree.lua
+++ nvim/.config/nvim/lua/felixws/plugins/neotree.lua
@@ -2,7 +2,7 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 require("neo-tree").setup({
-	close_if_last_window = true,
+	close_if_last_window = false,
 	popup_border_style = "rounded",
 	window = {
 		width = 35,
