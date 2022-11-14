ab8ded459fb95365f966cdf162697c489987ed95
Felix Scherz <felix.scherz@exxeta.com>
2022-11-14 17:47:38 +0100

    refactor tokyonight and lualine setup

--- nvim/.config/nvim/after/plugin/color.lua
+++ nvim/.config/nvim/after/plugin/color.lua
@@ -1,28 +1,3 @@
-require("tokyonight").setup({
-	transparent = true,
-    styles = {sidebars = "transparent"},
-    dim_inactive = true,
-    on_colors = function(colors)
-        colors.border = colors.blue
-		colors.diff.delete = colors.git.delete
-		colors.bg_highlight = "NONE"
-	end,
-	on_highlights = function(hl, _)
-		hl.CursorLine = {
-			color = hl.CursorLine.color,
-			bold = true
-		}
-	end
-})
-
-require("lualine").setup({
-	options = {
-		-- ... your lualine config
-		theme = "tokyonight",
-		-- ... your lualine config
-	},
-})
-
 vim.cmd("colorscheme tokyonight")
 
 vim.api.nvim_set_hl(0, "Search", { link = "IncSearch" })
