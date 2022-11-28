328fe9ad1959a3c53ab24daf01cb18e9f6b5bda2
Felix Scherz <felix.scherz@exxeta.com>
2022-11-28 09:06:20 +0100

    disable mini.cursorword for now

--- nvim/.config/nvim/after/plugin/color.lua
+++ nvim/.config/nvim/after/plugin/color.lua
@@ -1,3 +1,7 @@
 vim.cmd("colorscheme tokyonight")
 
 vim.api.nvim_set_hl(0, "Search", { link = "IncSearch" })
+-- vim.api.nvim_set_hl(0, "MiniCursorword", {guibg="#1f2335"})
+-- vim.cmd[[
+-- hi! MiniCursorwordCurrent gui=nocombine guifg=NONE guibg=NONE
+-- ]]
--- nvim/.config/nvim/lua/felixws/plugins/mini.lua
+++ nvim/.config/nvim/lua/felixws/plugins/mini.lua
@@ -1,1 +1,2 @@
 require('mini.comment').setup()
+--require('mini.cursorword').setup({ delay = 100, })
