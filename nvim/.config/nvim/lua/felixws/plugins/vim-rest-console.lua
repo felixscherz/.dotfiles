339465fac2ffe27ee2fbadcefb302906a4f57264
Felix Scherz <felix.scherz@exxeta.com>
2022-11-28 09:06:02 +0100

    disable vim rest console show curl command

--- nvim/.config/nvim/lua/felixws/plugins/vim-rest-console.lua
+++ nvim/.config/nvim/lua/felixws/plugins/vim-rest-console.lua
@@ -1,4 +1,4 @@
-vim.cmd[[ let g:vrc_curl_opts = {'-s': ''}]]
+--vim.cmd[[ let g:vrc_curl_opts = {'-s': ''}]]
 vim.cmd[[
     let g:vrc_auto_format_response_enabled = 1
     let s:vrc_auto_format_response_patterns = {
@@ -6,5 +6,5 @@ vim.cmd[[
       \ 'xml': 'xmllint --format -',
     \}
     let g:vrc_response_default_content_type = "application/json"
-    let g:vrc_show_command = 1
+    let g:vrc_show_command = 0
 ]]
