29d2069eed422ea80d3669de292681231a012c06
Felix Scherz <felix.scherz@googlemail.com>
2023-02-18 00:22:01 +0100

    fix search settings smartcase

--- nvim/.config/nvim/lua/felixws/set.lua
+++ nvim/.config/nvim/lua/felixws/set.lua
@@ -8,6 +8,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.opt.smartindent = true
