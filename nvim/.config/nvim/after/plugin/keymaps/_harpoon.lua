30bbc17cfad4a27422121dfd88329786c38f9d8d
Felix Scherz <felix.scherz@googlemail.com>
2023-03-12 13:32:41 +0100

    add prefix for harpoon commands

--- nvim/.config/nvim/after/plugin/keymaps/_harpoon.lua
+++ nvim/.config/nvim/after/plugin/keymaps/_harpoon.lua
@@ -1,8 +1,18 @@
 local wk = require("which-key")
 wk.register({
 	name = "harpoon",
-	a = { function() require("harpoon.mark").add_file() end, "add harpoon mark" },
-	w = { function() require("harpoon.ui").toggle_quick_menu() end, "harpoon ui" },
-	i = { function() require("harpoon.ui").nav_next() end, "harpoon next" },
-	o = { function() require("harpoon.ui").nav_prev() end, "harpoon prev" },
+	h = {
+		a = {
+			function()
+				require("harpoon.mark").add_file()
+			end,
+			"add harpoon mark",
+		},
+		w = {
+			function()
+				require("harpoon.ui").toggle_quick_menu()
+			end,
+			"harpoon ui",
+		},
+	},
 }, { prefix = "<leader>" })
