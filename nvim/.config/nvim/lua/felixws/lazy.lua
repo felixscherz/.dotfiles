a9fb7fb3d30a9308d255c8ab0f7e951d387a8a47
Felix Scherz <felix.scherz@googlemail.com>
2023-03-12 13:37:24 +0100

    fix bug specifying location as a list

--- nvim/.config/nvim/lua/felixws/lazy.lua
+++ nvim/.config/nvim/lua/felixws/lazy.lua
@@ -139,8 +139,8 @@ require("lazy").setup({
         config = function()
             require("orgmode").setup_ts_grammar()
             require("orgmode").setup({
-                org_agenda_files = { "~/org/**/*" },
-                org_default_notes_file = { "~/org/refile.org" },
+                org_agenda_files =  {"~/org/**/*"} ,
+                org_default_notes_file =  "~/org/refile.org" ,
             })
         end,
     },
