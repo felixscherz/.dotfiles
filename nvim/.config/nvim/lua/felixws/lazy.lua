be891f74e29255c26a20511ed39e81944df871a3
Felix Scherz <felix.scherz@googlemail.com>
2023-03-20 11:18:18 +0100

    add capture templates for org

--- nvim/.config/nvim/lua/felixws/lazy.lua
+++ nvim/.config/nvim/lua/felixws/lazy.lua
@@ -152,6 +152,11 @@ require("lazy").setup({
 			require("orgmode").setup({
 				org_agenda_files = { "~/org/**/*", "~/org/refile.org" },
 				org_default_notes_file = "~/org/refile.org",
+				org_capture_templates = {
+					t = { description = "Task", template = "* TODO %?\n  %u\n  %a" , target='~/org/todos/todos.org'},
+					m = { description = "Meeting", template = "* MEETING with %? :meeting:\n  %U", target='~/org/meetings/meetings.org' },
+					c = { description = "Code", template = "* CODE %? :code:\n  %U\n%a\n#+begin_src\n%x\n#+end_src", target='~/org/code/code.org' },
+				},
 			})
 		end,
 		lazy = false,
