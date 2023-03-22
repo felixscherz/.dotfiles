c7738a61768d87354dd1a555f446223b4e61818c
Felix Scherz <felix.scherz@googlemail.com>
2023-03-22 09:21:44 +0100

    change meeting template orgmode

--- nvim/.config/nvim/lua/felixws/lazy.lua
+++ nvim/.config/nvim/lua/felixws/lazy.lua
@@ -154,7 +154,7 @@ require("lazy").setup({
 				org_default_notes_file = "~/org/refile.org",
 				org_capture_templates = {
 					t = { description = "Task", template = "* TODO %?\n  %u\n  %a" , target='~/org/todos/todos.org'},
-					m = { description = "Meeting", template = "* MEETING with %? :meeting:\n  %U", target='~/org/meetings/meetings.org' },
+					m = { description = "Meeting", template = "* MEETING %? :meeting:\n  %U", target='~/org/meetings/meetings.org' },
 					c = { description = "Code", template = "* CODE %? :code:\n  %U\n%a\n#+begin_src\n%x\n#+end_src", target='~/org/code/code.org' },
 				},
 			})
