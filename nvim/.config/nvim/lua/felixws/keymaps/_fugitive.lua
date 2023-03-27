8fb56d4377d2c23b87f3850c5c6ae8e6314b62b8
Felix Scherz <felix.scherz@googlemail.com>
2023-03-27 11:28:49 +0200

    add hotkesy for git

--- nvim/.config/nvim/lua/felixws/keymaps/_fugitive.lua
+++ nvim/.config/nvim/lua/felixws/keymaps/_fugitive.lua
@@ -1,5 +1,18 @@
 local wk = require("which-key")
 wk.register({
+	name = "diffget",
 	["gdh"] = { ":diffget //2<CR>", "get left diff" },
 	["gdl"] = { ":diffget //3<CR>", "get right diff" },
 })
+
+wk.register({
+	name = "git",
+	g = {
+		["pu"] = { ":Git push<CR>", "git push" },
+		["pd"] = { ":Git pull<CR>", "git pull" },
+		["c"] = { ":Git commit<CR>", "git commit" },
+		["cm"] = { ":Git commit -m ", "git commit -m" },
+		["ca"] = { ":Git commit --amend --no-edit<CR>", "git commit amend" },
+		s = { ":Git switch ", "git switch" },
+	},
+}, { prefix = "<leader>" })
