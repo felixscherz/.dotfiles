f37b733dcb029f2ec27f387e10df8d77c35747d8
Felix Scherz <felix.scherz@googlemail.com>
2023-04-15 14:23:17 +0200

    fix: nvim-autopairs not initliazing

--- nvim/.config/nvim/lua/felixws/lazy.lua
+++ nvim/.config/nvim/lua/felixws/lazy.lua
@@ -40,6 +40,9 @@ require("lazy").setup({
 
 	{
 		"windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup()
+        end,
 		event = { "BufReadPre", "BufNewFile", "InsertEnter" },
 	},
 
