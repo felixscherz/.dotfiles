34f640d70c6e94edc5956a84b83fd703818af27d
Felix Scherz <felix.scherz@googlemail.com>
2023-06-15 09:32:18 +0200

    update zenmode settings

--- nvim/.config/nvim/lua/felixws/lazy.lua
+++ nvim/.config/nvim/lua/felixws/lazy.lua
@@ -234,7 +234,22 @@ require("lazy").setup({
 	{
 		"folke/zen-mode.nvim",
 		config = function()
-			require("zen-mode").setup()
+			require("zen-mode").setup({
+				window = {
+					height = 0.8,
+                    options = {
+                        number = true,
+                    }
+				},
+				plugins = {
+					twilight = { enabled = false },
+					tmux = { enabled = true },
+					alacritty = {
+						enabled = true,
+						font = "24.0",
+					},
+				},
+			})
 		end,
 		cmd = "ZenMode",
 	},
@@ -253,29 +268,29 @@ require("lazy").setup({
 		"jackMort/ChatGPT.nvim",
 		event = "VeryLazy",
 		config = function()
-            if os.getenv("OPENAI_API_KEY") ~= nil then
-                require("chatgpt").setup({
-                    openai_params = { max_tokens = 1000 },
-                    chat = {
-                        keymaps = {
-                            close = { "<C-c>" },
-                            yank_last = "<C-y>",
-                            yank_last_code = "<C-k>",
-                            scroll_up = "<C-u>",
-                            scroll_down = "<C-d>",
-                            toggle_settings = "<C-o>",
-                            new_session = "<C-n>",
-                            cycle_windows = "<Tab>",
-                            select_session = "<Enter>",
-                            rename_session = "r",
-                            delete_session = "d",
-                        },
-                    },
-                    popup_input = {
-                        submit = "<C-s>",
-                    },
-                })
-            end
+			if os.getenv("OPENAI_API_KEY") ~= nil then
+				require("chatgpt").setup({
+					openai_params = { max_tokens = 1000 },
+					chat = {
+						keymaps = {
+							close = { "<C-c>" },
+							yank_last = "<C-y>",
+							yank_last_code = "<C-k>",
+							scroll_up = "<C-u>",
+							scroll_down = "<C-d>",
+							toggle_settings = "<C-o>",
+							new_session = "<C-n>",
+							cycle_windows = "<Tab>",
+							select_session = "<Enter>",
+							rename_session = "r",
+							delete_session = "d",
+						},
+					},
+					popup_input = {
+						submit = "<C-s>",
+					},
+				})
+			end
 		end,
 		dependencies = {
 			"MunifTanjim/nui.nvim",
