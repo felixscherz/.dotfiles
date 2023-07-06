bb3a620301bd904146665cf4f3ff92a9b9e2cf9e
Felix Scherz <felix.scherz@googlemail.com>
2023-07-06 23:00:59 +0200

    set pytest as default runner for python dap

--- nvim/.config/nvim/lua/felixws/ide/dap/python.lua
+++ nvim/.config/nvim/lua/felixws/ide/dap/python.lua
@@ -1,7 +1,11 @@
 local M = {}
 
+
 function M.setup(_)
-  require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+
+    require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+    require("dap-python").test_runner = "pytest"
+
 end
 
 return M
