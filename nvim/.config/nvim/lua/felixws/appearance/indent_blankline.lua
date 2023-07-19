e22421aed7ae8e9075cf5e7e364240924325ff04
Felix Scherz <felix.scherz@googlemail.com>
2023-07-19 09:01:46 +0200

    add excluded filetypes to indent_blankline

--- nvim/.config/nvim/lua/felixws/appearance/indent_blankline.lua
+++ nvim/.config/nvim/lua/felixws/appearance/indent_blankline.lua
@@ -3,7 +3,16 @@ local M = {}
 M.config = function()
 	require("indent_blankline").setup({
 		show_current_context = true,
-        filetype_exclude = {'fugitive'}
+		filetype_exclude = {
+			"fugitive",
+			"git",
+			"neo-tree",
+			"lspinfo",
+			"checkhealth",
+			"help",
+			"man",
+			"",
+		},
 	})
 end
 
