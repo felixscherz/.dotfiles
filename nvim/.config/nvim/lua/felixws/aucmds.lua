ee4897f60fc218ce5460a330a3d3bd190e1e3fd1
Felix Scherz <felix.scherz@googlemail.com>
2023-07-26 21:13:30 +0200

    handle autocommands with lua api

--- nvim/.config/nvim/lua/felixws/aucmds.lua
+++ nvim/.config/nvim/lua/felixws/aucmds.lua
@@ -1,12 +1,22 @@
-local cmd = vim.cmd
+local function augroup(name)
+  return vim.api.nvim_create_augroup("felixws_" .. name, { clear = true })
+end
 
 -- Highlight on yank
-cmd([[
-  augroup YankHighlight
-    autocmd!
-    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-  augroup end
-]])
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
 
 vim.filetype.add({
 	filename = {
