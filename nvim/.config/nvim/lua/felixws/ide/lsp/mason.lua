457a1e3ee9cd21618fb500ae5dd9287d27073f1f
Felix Scherz <felix.scherz@googlemail.com>
2023-07-30 23:59:10 +0200

    move mason config to lazy.lua

--- nvim/.config/nvim/lua/felixws/ide/lsp/mason.lua
+++ nvim/.config/nvim/lua/felixws/ide/lsp/mason.lua
@@ -7,20 +7,6 @@ local servers = {
 	"yamlls",
 }
 
-local settings = {
-	ui = {
-		border = "rounded",
-		icons = {
-			package_installed = "◍",
-			package_pending = "◍",
-			package_uninstalled = "◍",
-		},
-	},
-	log_level = vim.log.levels.INFO,
-	max_concurrent_installers = 4,
-}
-
-require("mason").setup(settings)
 require("mason-lspconfig").setup({
 	ensure_installed = servers,
 	automatic_installation = true,
--- nvim/.config/nvim/lua/felixws/lazy.lua
+++ nvim/.config/nvim/lua/felixws/lazy.lua
@@ -169,10 +169,23 @@ require("lazy").setup({
 	{
 		"williamboman/mason.nvim",
 		cmd = "Mason",
+		config = function(_, opts)
+			require("mason").setup(opts)
+		end,
+		opts = {
+			ui = {
+				border = "rounded",
+				icons = {
+					package_installed = "◍",
+					package_pending = "◍",
+					package_uninstalled = "◍",
+				},
+			},
+			log_level = vim.log.levels.INFO,
+			max_concurrent_installers = 4,
+		},
 	}, -- simple to use language server installer
-	{ "williamboman/mason-lspconfig.nvim" }, -- simple to use language server installer
-
-	-- treesitter
+	{ "williamboman/mason-lspconfig.nvim" },
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
