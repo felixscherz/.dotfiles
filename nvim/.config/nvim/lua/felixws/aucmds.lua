c7748b13b4fbac0b660f861e21d5361c89c0f629
Felix Scherz <felixwscherz@gmail.com>
2024-02-21 19:22:55 +0100

    feat: aucmds to set terraform filetype

--- nvim/.config/nvim/lua/felixws/aucmds.lua
+++ nvim/.config/nvim/lua/felixws/aucmds.lua
@@ -23,3 +23,12 @@ vim.filetype.add({
 		[".envrc"] = "bash",
 	},
 })
+
+--fix terraform and hcl comment string
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+	callback = function(ev)
+		vim.bo[ev.buf].commentstring = "# %s"
+	end,
+	pattern = { "terraform", "hcl" },
+})
