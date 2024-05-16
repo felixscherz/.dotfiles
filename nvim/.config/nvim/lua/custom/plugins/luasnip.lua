213bdf0411ecd873eb506ec8c060d2e3bf8c07bb
Felix Scherz <felixwscherz@gmail.com>
2024-05-16 23:12:29 +0200

    feat: enable friendly-snippets

--- nvim/.config/nvim/lua/custom/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/custom/plugins/luasnip.lua
@@ -2,4 +2,8 @@ return {
 	"L3MON4D3/LuaSnip",
 	dependencies = { "rafamadriz/friendly-snippets" },
 	event = { "InsertEnter" },
+	config = function()
+		require("luasnip").setup()
+		require("luasnip.loaders.from_vscode").lazy_load()
+	end,
 }
