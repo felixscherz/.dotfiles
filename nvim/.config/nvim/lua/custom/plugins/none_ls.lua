f7672f4d81f133d1908bfa7ec509c128ca12c58f
Felix Scherz <felixwscherz@gmail.com>
2024-06-25 13:54:27 +0200

    feat: configure docformat formatter

--- nvim/.config/nvim/lua/custom/plugins/none_ls.lua
+++ nvim/.config/nvim/lua/custom/plugins/none_ls.lua
@@ -4,6 +4,22 @@ return {
 	config = function()
 		local null_ls = require("null-ls")
 		local formatting = null_ls.builtins.formatting
+		local docformat_source = {
+			name = "docformat",
+			filetypes = { "python" },
+			method = null_ls.methods.FORMATTING,
+			generator = {
+				fn = function(params)
+					local obj = vim.system(
+						{ "docformatter", "--style", "numpy", "--pre-summary-newline", "-" },
+						{ stdin = params.content }
+					):wait()
+					return { {
+						text = obj.stdout,
+					} }
+				end,
+			},
+		}
 		null_ls.setup({
 			debug = false,
 			sources = {
@@ -13,6 +29,7 @@ return {
 				formatting.black.with({ extra_args = { "--line-length", "120" } }),
 				formatting.stylua,
 				formatting.isort.with({ extra_args = { "--sl", "-l 120" } }),
+				docformat_source,
 			},
 		})
 	end,
