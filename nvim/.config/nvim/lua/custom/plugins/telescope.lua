7189b331cbfabae44e047de1640dfaf856c1fe85
Felix Scherz <felixwscherz@gmail.com>
2024-07-26 16:42:12 +0200

    chore: update telescope mappings

--- nvim/.config/nvim/lua/custom/plugins/telescope.lua
+++ nvim/.config/nvim/lua/custom/plugins/telescope.lua
@@ -35,43 +35,44 @@ return {
 		require("telescope").setup(opts)
 		local wk = require("which-key")
 
-		wk.register({
-			f = {
-				t = {
-					":Telescope<CR>",
-					"run telescope",
-				},
-				f = {
-					function()
-						require("telescope.builtin").find_files()
-					end,
-					"find files",
-				},
-				g = {
-					function()
-						require("telescope.builtin").live_grep()
-					end,
-					"live grep",
-				},
-				b = {
-					function()
-						require("telescope.builtin").buffers()
-					end,
-					"search buffers",
-				},
-				h = {
-					function()
-						require("telescope.builtin").help_tags()
-					end,
-					"serach tags",
-				},
-				G = {
-					function()
-						require("telescope").extensions.advanced_git_search.show_custom_functions()
-					end,
-					"git search",
-				},
+		wk.add({
+
+			{
+				"<leader>fG",
+				function()
+					require("telescope").extensions.advanced_git_search.show_custom_functions()
+				end,
+				desc = "git search",
 			},
-		}, { prefix = "<leader>" })
+			{
+				"<leader>fb",
+				function()
+					require("telescope.builtin").buffers()
+				end,
+				desc = "search buffers",
+			},
+			{
+				"<leader>ff",
+				function()
+					require("telescope.builtin").find_files()
+				end,
+				desc = "find files",
+			},
+			{
+				"<leader>fg",
+				function()
+					require("telescope.builtin").live_grep()
+				end,
+				desc = "live grep",
+			},
+			{
+				"<leader>fh",
+				function()
+					require("telescope.builtin").help_tags()
+				end,
+				desc = "serach tags",
+			},
+			{ "<leader>ft", ":Telescope<CR>", desc = "run telescope" },
+		})
 	end,
 }
