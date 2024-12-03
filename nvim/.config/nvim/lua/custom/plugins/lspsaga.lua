43f63b6edf19d84ffa83705dfaa34e2aab9f815f
Felix Scherz <felixwscherz@gmail.com>
2024-12-03 11:43:53 +0100

    feat: improve hover documentation wiht lspsaga

--- nvim/.config/nvim/lua/custom/plugins/lspsaga.lua
+++ nvim/.config/nvim/lua/custom/plugins/lspsaga.lua
@@ -1,6 +1,12 @@
 return {
 	"nvimdev/lspsaga.nvim",
-	opts = { lightbulb = { enable = false } },
+	opts = {
+		lightbulb = { enable = false },
+		hover = {
+			max_width = 0.6,
+			max_height = 0.3,
+		},
+	},
 	event = "LspAttach",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
