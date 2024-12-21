98ecbe63eb9d7bcc5c378b546f1f966c2c45a3a7
Felix Scherz <felixwscherz@gmail.com>
2024-12-21 22:42:23 +0100

    feat: use ruff in favour of ruff_lsp

--- nvim/.config/nvim/lua/custom/plugins/nvim_lspconfig.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim_lspconfig.lua
@@ -18,8 +18,7 @@ local function lsp_keymaps(bufnr)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gd", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gk", "<cmd>Lspsaga hover_doc ++keep<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
@@ -92,7 +91,7 @@ return {
 			"basedpyright",
 			"jsonls",
 			"rust_analyzer",
-			"ruff_lsp",
+			"ruff",
 			"yamlls",
 			"bashls",
 			"clangd",
