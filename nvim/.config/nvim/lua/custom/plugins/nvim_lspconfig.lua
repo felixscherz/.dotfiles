f22bd185b3c7a7d738e249da046e45407ddc3197
Felix Scherz <felixwscherz@gmail.com>
2025-02-06 13:32:16 +0100

    fix: silence server cancelled the request

--- nvim/.config/nvim/lua/custom/plugins/nvim_lspconfig.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim_lspconfig.lua
@@ -119,6 +119,17 @@ return {
 			"gopls",
 		}
 
+		-- silence Server cancelled the request warning that happens with rust_analyzer: https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
+		for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+			local default_diagnostic_handler = vim.lsp.handlers[method]
+			vim.lsp.handlers[method] = function(err, result, context, _config)
+				if err ~= nil and err.code == -32802 then
+					return
+				end
+				return default_diagnostic_handler(err, result, context, _config)
+			end
+		end
+
 		require("mason-lspconfig").setup({
 			ensure_installed = servers,
 			automatic_installation = true,
