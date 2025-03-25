8abc63478ccc3a8950a563a047ee7c792c87ad3a
Felix Scherz <felixwscherz@gmail.com>
2025-03-25 14:33:40 +0100

    feat: copy current filepath to clipboard function

---
+++ nvim/.config/nvim/plugin/functions.lua
@@ -0,0 +1,6 @@
+local function insertFullPath()
+	local filepath = vim.fn.expand("%")
+	vim.fn.setreg("+", filepath) -- write to clippoard
+end
+
+vim.keymap.set("n", "<leader>pc", insertFullPath, { noremap = true, silent = true })
