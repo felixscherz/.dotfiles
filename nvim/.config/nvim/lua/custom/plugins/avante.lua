95508c971d9aacb081321c1c9643fb44d1c404d6
Felix Scherz <felixwscherz@gmail.com>
2025-05-13 11:07:52 +0200

    feat: set avant version

--- nvim/.config/nvim/lua/custom/plugins/avante.lua
+++ nvim/.config/nvim/lua/custom/plugins/avante.lua
@@ -1,7 +1,7 @@
 return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
-	version = "v0.0.22", -- Never set this value to "*"! Never!
+	version = false, -- Never set this value to "*"! Never!
 	opts = {
 		-- add any opts here
 		-- for example
