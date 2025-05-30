584a677f40e1231add80a7c76d03d43e984baa04
Felix Scherz <felixwscherz@gmail.com>
2025-05-30 18:44:10 +0200

    feat: add asyncapi for yamlls

--- nvim/.config/nvim/lua/custom/lspconfig/yamlls.lua
+++ nvim/.config/nvim/lua/custom/lspconfig/yamlls.lua
@@ -1,3 +1,17 @@
+---
+---
+local schemastore_schemas = require("schemastore").yaml.schemas()
+local schema_overrides = {
+	["https://raw.githubusercontent.com/asyncapi/spec-json-schemas/refs/heads/master/schemas/2.0.0.json"] = "asyncapi.yaml",
+	["https://raw.githubusercontent.com/asyncapi/spec-json-schemas/refs/heads/master/schemas/2.1.0.json"] = "asyncapi.yaml",
+	["https://raw.githubusercontent.com/asyncapi/spec-json-schemas/refs/heads/master/schemas/2.2.0.json"] = "asyncapi.yaml",
+	["https://raw.githubusercontent.com/asyncapi/spec-json-schemas/refs/heads/master/schemas/2.3.0.json"] = "asyncapi.yaml",
+	["https://raw.githubusercontent.com/asyncapi/spec-json-schemas/refs/heads/master/schemas/2.4.0.json"] = "asyncapi.yaml",
+	["https://raw.githubusercontent.com/asyncapi/spec-json-schemas/refs/heads/master/schemas/2.5.0.json"] = "asyncapi.yaml",
+	["https://raw.githubusercontent.com/asyncapi/spec-json-schemas/refs/heads/master/schemas/2.6.0.json"] = "asyncapi.yaml",
+	["https://raw.githubusercontent.com/asyncapi/spec-json-schemas/refs/heads/master/schemas/3.0.0.json"] = "asyncapi.yaml",
+}
+
 ---@type vim.lsp.Config
 return {
 	settings = {
@@ -6,7 +20,7 @@ return {
 				enable = false,
 				url = "",
 			},
-			schemas = require("schemastore").yaml.schemas(),
+			schemas = vim.tbl_extend("force", schemastore_schemas, schema_overrides),
 			hover = true,
 			validate = true,
 			completion = true,
