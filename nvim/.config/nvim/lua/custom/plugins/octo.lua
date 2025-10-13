af3f9981cc379376b406cd4ef9fcf2455156c3ab
Felix Scherz <felixwscherz@gmail.com>
2025-10-13 10:36:24 +0200

    feat: add octo as a plugin

---
+++ nvim/.config/nvim/lua/custom/plugins/octo.lua
@@ -0,0 +1,14 @@
+return {
+	"pwntester/octo.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+		-- OR 'ibhagwan/fzf-lua',
+		-- OR 'folke/snacks.nvim',
+		"nvim-tree/nvim-web-devicons",
+	},
+	cmd = { "Octo" },
+	config = function()
+		require("octo").setup()
+	end,
+}
