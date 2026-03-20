e1d00047d94699b05763188dfbde4f354794de85
Felix Scherz <felixwscherz@gmail.com>
2026-03-20 11:14:31 +0100

    chore: use default root_dir detection for basedpyright

--- nvim/.config/nvim/lua/custom/lspconfig/basedpyright.lua
+++ nvim/.config/nvim/lua/custom/lspconfig/basedpyright.lua
@@ -1,14 +1,5 @@
 ---@type vim.lsp.Config
 return {
-	root_dir = vim.fs.root(0, {
-		"pyproject.toml",
-		"setup.py",
-		"setup.cfg",
-		"requirements.txt",
-		"Pipfile",
-		"pyrightconfig.json",
-		".git",
-	}),
 	settings = {
 		basedpyright = {
 			typeCheckingMode = "standard",
