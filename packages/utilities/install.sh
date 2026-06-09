5a461f3b355d0fe8a6ff0974f930011b862e2ceb
Felix Scherz <felixwscherz@gmail.com>
2026-06-09 13:21:55 +0200

    feat: add gh to utilities

--- packages/utilities/install.sh
+++ packages/utilities/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 source "$(dirname "$0")/../../lib.sh"
-install_package fd fzf jq parallel direnv ripgrep awscli podman docker
+install_package fd fzf jq parallel direnv ripgrep awscli podman docker gh
