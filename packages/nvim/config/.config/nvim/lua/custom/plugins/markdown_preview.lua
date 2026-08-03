7d21d5447cc1dec60f89888b6ae306b8b2f91bc7
Felix Scherz <felixwscherz@gmail.com>
2026-08-03 15:06:55 +0200

    feat: add markdown preview plugin

---
+++ packages/nvim/config/.config/nvim/lua/custom/plugins/markdown_preview.lua
@@ -0,0 +1,15 @@
+return {
+  "selimacerbas/markdown-preview.nvim",
+  dependencies = { "selimacerbas/live-server.nvim" },
+  ft = { "markdown" },
+  config = function()
+    require("markdown_preview").setup({
+      -- all optional; sane defaults shown
+      instance_mode = "takeover",  -- "takeover" (one tab) or "multi" (tab per instance)
+      port = 0,                    -- 0 = auto (8421 for takeover, OS-assigned for multi)
+      open_browser = true,
+      default_theme = "dark",      -- "dark" or "light"; initial preview theme
+      debounce_ms = 300,
+    })
+  end,
+}
