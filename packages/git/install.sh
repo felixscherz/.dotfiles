a2a67dc92202e8436a068a70f7f06dc886c0e183
Felix Scherz <felixwscherz@gmail.com>
2026-09-15 13:43:42 +0200

    fix: gitignore setup

--- packages/git/install.sh
+++ packages/git/install.sh
@@ -2,6 +2,6 @@
 set -e
 source "$(dirname "$0")/../../lib.sh"
 # Stow global .gitignore
-stow_it git
+stow --dotfiles --dir="$DOTFILES_DIR/packages/git" --target="$HOME" config
 # Stow private .gitconfig
 stow --dir="$DOTFILES_DIR/packages/private" --target="$HOME" git
